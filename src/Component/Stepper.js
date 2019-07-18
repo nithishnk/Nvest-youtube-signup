db0b761cc05f7abd763871cec60dc85e8f535db0
nithishnk <nithish@nvestbank.com>
2019-07-18 16:02:19 +0530

    remhead

--- src/Component/Stepper.js
+++ src/Component/Stepper.js
@@ -80,7 +80,7 @@ export default function HorizontalLabelPositionBelowStepper() {
               </div>
               <div>
               <Button className="pos-btn-right" onClick={handleNext}>
-                {activeStep === steps.length - 1 ? 'Finish' : <i class="sze fa fa-chevron-right" aria-hidden="true"></i>}
+                {activeStep === steps.length - 1 ? '' : <i class="sze fa fa-chevron-right" aria-hidden="true"></i>}
               </Button>
             </div>
             </div>
